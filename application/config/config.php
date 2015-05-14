f3804e224d3d25c25a37a545424860f3c65980fa
maciejkaiser <maciejkaiser@hotmail.com>
2015-05-14 02:37:10 +0200

    Fixed problem with url
    Fixed problem with index.php in url

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ $config['base_url'] = 'http://localhost/Faktury2/faktury/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
