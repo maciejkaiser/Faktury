0a0ac4ae574e6991e3c9531371d875dc2b229d69
maciejkaiser <maciejkaiser@hotmail.com>
2015-05-15 19:08:11 +0200

    index page update

---
+++ application/views/home/index.php
@@ -0,0 +1,16 @@
+
+<hr>
+<div class="row">
+
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+
+     <div class="jumbotron">
+			<h1>Invoices App <small>0.1</small></h1>
+			<p>All your invoices in one place.</p>
+			<p><a class="btn btn-primary btn-lg" href="<?php echo base_url();?>user" role="button">Sign in</a></p>
+     </div>
+
+     <div class="well"><?php echo $content; ?></div>
+
+ </div>
+</div>
